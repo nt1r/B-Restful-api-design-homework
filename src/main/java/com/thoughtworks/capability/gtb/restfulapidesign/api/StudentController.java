52c298f97bca5bd8188fb7bacca323f41ec3b249
Leqi <michaelleqihust@gmail.com>
2020-09-14 01:09:56 +0800

    [leqi] feat: finish add student API.

--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/api/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/api/StudentController.java
@@ -1,7 +1,22 @@
 package com.thoughtworks.capability.gtb.restfulapidesign.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.thoughtworks.capability.gtb.restfulapidesign.entity.Student;
+import com.thoughtworks.capability.gtb.restfulapidesign.service.StudentService;
+import com.thoughtworks.capability.gtb.restfulapidesign.vo.StudentVo;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
+@RequestMapping("/students")
 public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping("")
+    public Student addOneStudent(@RequestBody StudentVo studentVo) {
+        return studentService.addOneStudent(studentVo);
+    }
 }
--- src/test/java/com/thoughtworks/capability/gtb/restfulapidesign/api/StudentControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/restfulapidesign/api/StudentControllerTest.java
@@ -1,11 +1,37 @@
 package com.thoughtworks.capability.gtb.restfulapidesign.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.capability.gtb.restfulapidesign.entity.Gender;
+import com.thoughtworks.capability.gtb.restfulapidesign.vo.StudentVo;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class StudentControllerTest {
+    private final String ADD_ONE_STUDENT_URL = "/students";
+
+    @Autowired
+    MockMvc mockMvc;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private StudentVo sampleStudentVo = StudentVo.builder().name("张三").gender(Gender.Male).note("班长").build();
 
     @BeforeEach
     void setUp() {
@@ -13,5 +39,18 @@ class StudentControllerTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    @Test
+    public void shouldAddOneStudent() throws Exception {
+        mockMvc.perform(post(ADD_ONE_STUDENT_URL).accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(sampleStudentVo)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is("0")))
+                .andExpect(jsonPath("$.name", is("张三")))
+                .andExpect(jsonPath("$.gender", is("Male")))
+                .andExpect(jsonPath("$.note", is("班长")));
     }
 }
